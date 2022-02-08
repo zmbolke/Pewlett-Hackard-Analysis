a3a4461272597f511d1e2ce8cbdc7c1d6095ad11
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-02-07 21:13:05 -0600

    QUERIES

--- queries.sql
+++ queries.sql
@@ -28,3 +28,9 @@ SELECT COUNT(first_name)
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+--Create new table of retirees
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
