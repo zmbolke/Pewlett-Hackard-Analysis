16dce0336e0245cfb341aec0838a9b32e34819d4
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-02-07 22:16:48 -0600

    JOINS, GROUPBY, COUNT, and EXPORT

--- queries.sql
+++ queries.sql
@@ -79,7 +79,21 @@ SELECT ri.emp_no,
 	ri.first_name,
 	ri.last_name,
 de.to_date
+INTO current_emp
 FROM retirement_info as ri 
 LEFT JOIN  dept_emp as de
 ON ri.emp_no = de.emp_no
 WHERE de.to_date = ('9999-01-01');
+--Employee count by department
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
+--Employee count by department with order
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
