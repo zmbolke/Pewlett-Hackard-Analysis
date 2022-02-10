a3209c5c9e29b8601be3839015a6559e5d9bc9c1
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-02-09 23:10:58 -0600

    Deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -29,3 +29,20 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC;
 
+--Mentor eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ASC;
