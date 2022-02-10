97b61db87c55bf4d2e991429a9825089e5f2b725
zmbolke <95301484+zmbolke@users.noreply.github.com>
2022-02-09 22:58:19 -0600

    Deliverable 1

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -12,7 +12,7 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no ASC;
 
---Use dustinct with orderby to remove duplicate rows
+--Use distinct with orderby to remove duplicate rows
 SELECT DISTINCT ON (emp_no) emp_no, 
 first_name,
 last_name,
@@ -22,4 +22,10 @@ FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
+--Count number of retirees by titles
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY COUNT(title) DESC;
 
